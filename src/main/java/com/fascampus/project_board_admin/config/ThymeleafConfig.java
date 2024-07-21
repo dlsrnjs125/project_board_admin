310aeff97bd2d693d468ce9bcd0c00b7469ec754
inkwon <dlsrnjs125@naver.com>
2024-07-22 03:14:19 +0900

    #4 - 스프링 부트 프로젝트 초기화
    #2 에서 분석했던 내용을 토대로
    스프링 부트 2.7.5 뼈대를 세움
    개발 준비 완료

---
+++ src/main/java/com/fascampus/project_board_admin/config/ThymeleafConfig.java
@@ -0,0 +1,25 @@
+package com.fascampus.project_board_admin.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.decoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    @ConfigurationProperties("spring.thymeleaf3")
+    public record Thymeleaf3Properties(boolean decoupledLogic) {}
+
+}
