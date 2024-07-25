66bc28098f4ddba9f6c8c1bba23a935b35d2d617
inkwon <dlsrnjs125@naver.com>
2024-07-25 17:26:40 +0900

    #17 - 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트

--- src/test/java/com/fascampus/project_board_admin/controller/AdminAccountControllerTest.java
+++ src/test/java/com/fascampus/project_board_admin/controller/AdminAccountControllerTest.java
@@ -1,14 +1,27 @@
 package com.fascampus.project_board_admin.controller;
 
 import com.fascampus.project_board_admin.config.SecurityConfig;
+import com.fascampus.project_board_admin.domain.constant.RoleType;
+import com.fascampus.project_board_admin.dto.AdminAccountDto;
+import com.fascampus.project_board_admin.service.AdminAccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,13 +32,24 @@ class AdminAccountControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean private AdminAccountService adminAccountService;
+
     public AdminAccountControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
+    @BeforeTestMethod
+    public void securitySetup() {
+        given(adminAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createAdminAccountDto()));
+        given(adminAccountService.saveUser(anyString(), anyString(), anySet(), anyString(), anyString(), anyString()))
+                .willReturn(createAdminAccountDto());
+    }
+
+    @WithMockUser(username = "tester", roles = "USER")
     @DisplayName("[view][GET] 어드민 회원 페이지 - 정상 호출")
     @Test
-    void givenNothing_whenRequestingAdminMembersView_thenReturnsAdminMembersView() throws Exception {
+    void givenAuthorizedUser_whenRequestingAdminMembersView_thenReturnsAdminMembersView() throws Exception {
         // Given
 
         // When & Then
@@ -33,6 +57,49 @@ class AdminAccountControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("admin/members"));
+    }
+
+    @WithMockUser(username = "tester", roles = "USER")
+    @DisplayName("[data][GET] 어드민 회원 리스트 - 정상 호출")
+    @Test
+    void givenAuthorizedUser_whenRequestingAdminMembers_thenReturnsAdminMembers() throws Exception {
+        // Given
+        given(adminAccountService.users()).willReturn(List.of());
+
+        // When & Then
+        mvc.perform(get("/api/admin/members"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+        then(adminAccountService).should().users();
+    }
+
+    @WithMockUser(username = "tester", roles = "MANAGER")
+    @DisplayName("[data][DELETE] 어드민 회원 삭제 - 정상 호출")
+    @Test
+    void givenAuthorizedUser_whenDeletingAdminMember_thenDeletesAdminMember() throws Exception {
+        // Given
+        String username = "Inkwon";
+        willDoNothing().given(adminAccountService).deleteUser(username);
+
+        // When & Then
+        mvc.perform(
+                        delete("/api/admin/members/" + username)
+                                .with(csrf())
+                )
+                .andExpect(status().isNoContent());
+        then(adminAccountService).should().deleteUser(username);
+    }
+
+
+    private AdminAccountDto createAdminAccountDto() {
+        return AdminAccountDto.of(
+                "InkwonTest",
+                "pw",
+                Set.of(RoleType.USER),
+                "Inkwon-test@email.com",
+                "Inkwon-test",
+                "test memo"
+        );
     }
 
 }
