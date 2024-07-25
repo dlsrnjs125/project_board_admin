a7b71b4ebd079773dfd55218de18ce2475e0ce0d
inkwon <dlsrnjs125@naver.com>
2024-07-25 17:19:12 +0900

    #17 - TestSecurityConfig: 컨트롤러 테스트에 공통으로 들어갈 인증 셋업 추출
    이것으로 모든 controller test 에서 일일이
    `AdminAccountService`를 mocking 하거나,
    인증 세팅을 하는 코드를 표현하지 않아도 됨.
    각 컨트롤러 관심사에서 벗어나는 인증 코드 및 mocking이
    테스트 코드에 드러나지 않게끔 도와줌.

--- src/test/java/com/fascampus/project_board_admin/controller/ArticleManagementControllerTest.java
+++ src/test/java/com/fascampus/project_board_admin/controller/ArticleManagementControllerTest.java
@@ -1,7 +1,6 @@
 package com.fascampus.project_board_admin.controller;
 
-import com.fascampus.project_board_admin.config.SecurityConfig;
-import com.fascampus.project_board_admin.domain.constant.RoleType;
+import com.fascampus.project_board_admin.config.TestSecurityConfig;
 import com.fascampus.project_board_admin.dto.ArticleDto;
 import com.fascampus.project_board_admin.dto.UserAccountDto;
 import com.fascampus.project_board_admin.service.ArticleManagementService;
@@ -16,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -25,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("컨트롤러 - 게시글 관리")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(ArticleManagementController.class)
 class ArticleManagementControllerTest {
 
--- src/test/java/com/fascampus/project_board_admin/controller/MainControllerTest.java
+++ src/test/java/com/fascampus/project_board_admin/controller/MainControllerTest.java
@@ -1,6 +1,6 @@
 package com.fascampus.project_board_admin.controller;
 
-import com.fascampus.project_board_admin.config.SecurityConfig;
+import com.fascampus.project_board_admin.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 루트 컨트롤러")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
