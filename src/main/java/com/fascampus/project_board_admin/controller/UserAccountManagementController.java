657674612b36277248f20c42a20c144c95b40486
inkwon <dlsrnjs125@naver.com>
2024-07-29 18:45:56 +0900

    #20 - 회원 관리 기능 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/fascampus/project_board_admin/controller/UserAccountManagementController.java
+++ src/main/java/com/fascampus/project_board_admin/controller/UserAccountManagementController.java
@@ -1,23 +1,43 @@
 package com.fascampus.project_board_admin.controller;
 
+import com.fascampus.project_board_admin.dto.response.UserAccountResponse;
+import com.fascampus.project_board_admin.service.UserAccountManagementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
-    public String userAccounts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+    public String userAccounts(Model model) {
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
+
         return "management/user-accounts";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
     }
 
 }
